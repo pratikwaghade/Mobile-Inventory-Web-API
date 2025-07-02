ecd3122b68e93eac4d3bf0618515e3775ff8d144
pratikwaghade <pratikwaghade31@gmail.com>
2025-07-02 17:12:11 +0530

    Add project files.

---
+++ Data/MobileContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mobile_Inventory_Web_API.Model;
+
+namespace Mobile_Inventory_Web_API.Data
+{
+    public class MobileContext : DbContext
+    {
+        public DbSet<Mobile> Mobiles { get; set; }
+        public MobileContext(DbContextOptions<MobileContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            
+            string connnectionString = @"Data Source = .\SQLEXPRESS; Initial Catalog = MobileDB; TrustServerCertificate = True; Integrated Security = True";
+            optionsBuilder.UseSqlServer(connnectionString);
+
+
+        }
+    }
+}
---
+++ Model/Mobile.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mobile_Inventory_Web_API.Model
+{
+    public class Mobile
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Brand { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
